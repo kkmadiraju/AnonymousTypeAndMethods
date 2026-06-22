c9c1dc5e786ed37a64531cd024ee058d59cb3cb6
Kiran Kumar Madiraju <ssamadiraju@gmail.com>
2026-06-22 21:10:02 +0530

    Add project files.

---
+++ AnonymousTypeAndMethods/Program.cs
@@ -0,0 +1,11 @@
+﻿//Console.WriteLine("Hello, World!");
+var employee = new
+{
+    Id = 101,
+    Name = "Kiran",
+    Department = "IT"
+};
+
+Console.WriteLine(employee.Id);
+Console.WriteLine(employee.Name);
+Console.WriteLine(employee.Department);
